146e320991770b4d22871ed1b1ec8c535bb16025
Leandro Lima de Andrade <70245443+Elacosta@users.noreply.github.com>
2023-03-27 21:57:32 -0300

    Update desafio.kt
    Adicionadas as respostas ao desafio de Kotlin

--- desafio.kt
+++ desafio.kt
@@ -1,21 +1,31 @@
-// [Template no Kotlin Playground](https://pl.kotl.in/WcteahpyN)
-
 enum class Nivel { BASICO, INTERMEDIARIO, DIFICIL }
 
-class Usuario
+class Usuario(val nome: String)
 
 data class ConteudoEducacional(var nome: String, val duracao: Int = 60)
 
-data class Formacao(val nome: String, var conteudos: List<ConteudoEducacional>) {
+data class Formacao(val nome: String, var conteudos: List<ConteudoEducacional>, var nivel: Nivel = Nivel.BASICO) {
 
     val inscritos = mutableListOf<Usuario>()
     
     fun matricular(usuario: Usuario) {
-        TODO("Utilize o parâmetro $usuario para simular uma matrícula (usar a lista de $inscritos).")
+     inscritos.add(usuario) //Faz a adição de um Usuario na MutableList de Inscritos
     }
 }
 
 fun main() {
-    TODO("Analise as classes modeladas para este domínio de aplicação e pense em formas de evoluí-las.")
-    TODO("Simule alguns cenários de teste. Para isso, crie alguns objetos usando as classes em questão.")
+    val user1 = Usuario("Johan")
+    val user2 = Usuario("Johanna")
+    
+    var content1 = ConteudoEducacional("Introdução a Programação", 15)
+    var content2 = ConteudoEducacional("Programação OO", 35)
+    
+    var formacao1 = Formacao("Desenvolvimento de Software", listOf(content1, content2), Nivel.BASICO)
+    
+    
+    formacao1.matricular(user2)
+    formacao1.matricular(user1)
+    
+    println("O usuario ${user1} está na formação: ${formacao1}")
+    
 }
